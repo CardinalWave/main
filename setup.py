9fc3c2ae291ba24468ce9f657fdb7ea735dfe30c
vmarquezv <vmarquezv@debian>
2024-09-08 11:05:36 -0300

    feat: Create setup script

---
+++ setup.py
@@ -0,0 +1,56 @@
+import os
+import shutil
+import subprocess
+
+DOCKER_COMPOSE_FILE = 'docker-compose.yml'
+
+REPOSITORIES = [
+    {'url': 'git@github.com:CardinalWave/mqtt-mosquitto.git', 'branch': 'main'},
+    {'url': 'git@github.com:CardinalWave/nginx.git', 'branch': 'main'},
+    {'url': 'git@github.com:CardinalWave/cw_message_db.git', 'branch': 'main'},
+    {'url': 'git@github.com:CardinalWave/cw_central_db.git', 'branch': 'main'},
+    {'url': 'git@github.com:CardinalWave/cw-bff-service.git', 'branch': 'config_compose'},
+    {'url': 'git@github.com:CardinalWave/cw-log-trace.git', 'branch': 'config_compose'},
+    {'url': 'git@github.com:CardinalWave/cw-message-service.git', 'branch': 'config_compose'},
+    {'url': 'git@github.com:CardinalWave/cw-central-service.git', 'branch': 'config_compose'},
+    {'url': 'git@github.com:CardinalWave/cw-mqtt-gateway.git', 'branch': 'config_compose'},
+    {'url': 'git@github.com:CardinalWave/cw-auth-service.git', 'branch': 'main'},
+ ]
+
+DEST_DIR = 'cardinal_wave'
+
+if not os.path.exists(DEST_DIR):
+    os.makedirs(DEST_DIR)
+
+def clone_repositories(repos, dest_dir):
+    for repo in repos:
+        clone_url = repo['url']
+        branch = repo['branch']
+        repo_name = clone_url.split('/')[-1].replace('.git', '')
+        repo_path = os.path.join(dest_dir, repo_name)
+
+        if not os.path.exists(repo_path):
+            print(f'Clonando {repo_name} na branch {branch}...')
+            subprocess.run(['git', 'clone', clone_url, repo_path])
+            subprocess.run(['git', 'checkout', branch], cwd=repo_path)
+        else:
+            print(f'Clonando {repo_name} na branch {branch}...')
+            subprocess.run(['git', 'fetch'], cwd=repo_path)
+            subprocess.run(['git', 'checkout', branch], cwd=repo_path)
+
+def copy_docker_compose(target_path):
+    if os.path.isfile(DOCKER_COMPOSE_FILE):
+        shutil.copy(DOCKER_COMPOSE_FILE, target_path)
+        print(f'Arquivo {DOCKER_COMPOSE_FILE} copiado para {target_path}')
+    else:
+        print(f'O arquivo {DOCKER_COMPOSE_FILE} não foi encontrado no diretorio atual')
+
+def main():
+    print('Obtendo repositorios da lista...')
+    clone_repositories(REPOSITORIES, DEST_DIR)
+    print('Processo concluido.')
+    copy_docker_compose(DEST_DIR)
+
+ 
+if __name__ == '__main__':
+    main()
