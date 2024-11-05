d39c7cb81f5bfca592653b84f72941d734197599
vmarquezv <vmarquezv@debian.debian>
2024-11-05 19:29:41 -0300

    server: rename python project repo name

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ REPOSITORIES = [
     {'url': 'https://github.com/CardinalWave/cw-central-service.git', 'branch': 'master'},
     {'url': 'https://github.com/CardinalWave/cw-mqtt-gateway.git', 'branch': 'server'},
     {'url': 'https://github.com/CardinalWave/cw-auth-service.git', 'branch': 'master'},
-    {'url': 'https://github.com/CardinalWave/python-image-base.git', 'branch': 'main'},
+    {'url': 'https://github.com/CardinalWave/python-base-image.git', 'branch': 'main'},
  ]
 
 DEST_DIR = 'cardinal_wave'
